9837e596629d499a808c5f368239b2cf6cb29332
ivygod <karthik.k.mikkilineni@gmail.com>
2025-01-14 14:28:42 -0600

    Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
 from ultralytics iport YOLO
-
-model = YOLO("")
+// You Must download the file in the read me file
+model = YOLO("yolo11m-seg.pt")
+// add amount of epochs 
+model.train(source = "dataset_custom.yaml", batch = 8, workers = 0, device = "cpu", epochs = ?)
